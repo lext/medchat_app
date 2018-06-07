9a99e0e2a2659e6db9e11320d1aec850ccd2ec70
lext <aleksei.tiulpin@protonmail.ch>
2018-06-07 23:09:34 +0300

    connection to mongo from node

--- srv/app.js
+++ srv/app.js
@@ -2,6 +2,35 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+
+const findDocuments = function(db, coll_name, callback) {
+  // Get the documents collection
+  const collection = db.collection(coll_name);
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log("============================");
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  const db = client.db("medchat");
+  console.log("Connected successfully to DB server");
+  findDocuments(db, "people", ()=>{client.close()});
+  client.close();
+});
+
+
 io.on('connection', function(socket){
   console.log('Client connected');
   socket.on('auth', (data)=> {
