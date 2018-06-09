5e2b9575a22927be31c94397985a5dcb7563e473
lext <aleksei.tiulpin@protonmail.ch>
2018-06-09 13:02:42 +0300

    Proper android rebuild and refactoring of authentication

--- srv/app.js
+++ srv/app.js
@@ -1,6 +1,7 @@
- var app = require('express')();
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+require('console-stamp')(console, '[HH:MM:ss.l]');
 
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
@@ -8,6 +9,33 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 
 
+
+const retrieveAuthUserInfo = function(socket, channel, callback) {
+  socket.on(channel, (client_data)=> {
+    MongoClient.connect(url, function(err, client) {
+          // Connecting
+          assert.equal(null, err);
+          const db = client.db("medchat");
+          // Checking of the person is in the database
+          db.collection("people").findOne({"Ssn":client_data.ssn}, function(err, person) {
+            assert.equal(err, null);
+            // If the user is not in the database, we return 0 (Does not exist)
+            console.log('Checking if ' + client_data.ssn + ' is in the database');
+            if (person == null){
+              console.log('User '+ client_data.ssn + ' not found');
+              socket.emit('auth_result', {auth_code: 0});
+            } else {
+              db.collection("patients").findOne({"person": new mongo.ObjectId(person._id)}, function(err, patient_data) {
+                assert.equal(err, null);
+                assert.notEqual(patient_data, null);
+                // Calling the callback using the data retrived
+                callback(client, client_data, patient_data);
+              });
+            }
+        });
+    });
+  });
+};
 
 
 const registerAuth = function(socket) {
@@ -17,70 +45,29 @@ const registerAuth = function(socket) {
   3) User computes sha256(pass + salt) and sends it back to the server on channel <auth_pass>
   4) If the hashes match, the user gets auth_code=1
 
-  If the user id is not found, the user gets code 0 - does not exist
+  If the user id is not found, the user gets code auth_code=0 - does not exist
 
   */
-  socket.on('auth_init', (data)=> {
-    MongoClient.connect(url, function(err, client) {
-        // Connecting
-        assert.equal(null, err);
-        const db = client.db("medchat");
-        // Checking of the person is in the database
-        db.collection("people").findOne({"Ssn":data.ssn}, function(err, person) {
-          assert.equal(err, null);
-          // If the user is not in teh database, we return 0 (Does not exist)
-          if (person == null){
-            socket.emit('auth_result', {auth_code: 0});
-          } else {
-            // If the user exists we look for a salt value for a given patient and also retrievening the hashed password
-            db.collection("patients").findOne({"person": new mongo.ObjectId(person._id)}, function(err, patient) {
-              assert.equal(err, null);
-              assert.notEqual(patient, null);
-              // Now we know the salt for the password. We will send it back to the user
-              // If his hash(password+salt) is correct, we approve the login
-              socket.emit('auth_salt', {salt: patient.salt});
-              client.close();
-            });
-          }
-        });
-    });
+  retrieveAuthUserInfo(socket, 'auth_init', function(client, client_data, patient_data) {
+    socket.emit('auth_salt', {salt: patient_data.salt});
+    client.close();
   });
 
-
-
-  socket.on('auth_pass', (data)=> {
-    MongoClient.connect(url, function(err, client) {
-      // Connecting
-      assert.equal(null, err);
-      const db = client.db("medchat");
-      db.collection("people").findOne({"Ssn":data.ssn}, function(err, person) {
-      assert.equal(err, null);
-      // If the user is not in teh database, we return 0 (Does not exist)
-      if (person == null){
+  retrieveAuthUserInfo(socket, 'auth_pass', function(client, client_data, patient_data) {
+    if (patient_data.password == client_data.hash) {
+        console.log('Password for user '+ client_data.ssn +' is correct');
+        socket.emit('auth_result', {auth_code: 1});
+    } else {
+        console.log('Incorrect attempt of authentication for user '+ client_data.ssn +' (passwords don\'t match)');
         socket.emit('auth_result', {auth_code: 0});
-      } else {
-        // If the user exists we look for a salt value for a given patient and also retrievening the hashed password
-        db.collection("patients").findOne({"person": new mongo.ObjectId(person._id)}, function(err, patient) {
-          assert.equal(err, null);
-          assert.notEqual(patient, null);
-          // Now we know the salt for the password. We will send it back to the user
-          // If his hash(password+salt) is correct, we approve the login
-          console.log(patient.password);
-          console.log(data.hash);
-          console.log('===========================================');
-          client.close();
-        });
-        }
-      });
-    });
+    }
+    client.close();
   });
 };
 
 io.on('connection', function(socket){
     console.log('Client connected');
     registerAuth(socket);
-
-
 });
 
 http.listen(3000, function(){
