914ec3a1aba807784c350390c30da59fd0395b0c
lext <aleksei.tiulpin@protonmail.ch>
2018-06-12 23:47:26 +0300

    Added doctor UI chat. Started to build teh patients list

--- doctor_interface/src/App.js
+++ doctor_interface/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import Auth from './Auth'
-
+import Chat from './Chat'
 class App extends Component {
   constructor() {
     super()
 
     const state = {
-      display_current: 'AUTH',
+      display_current: 'CHAT',
     };
 
     this.state = state;
   }
 
   handleAuth(auth_result){
-    console.log(auth_result);
+    if (auth_result.auth_code === 1) {
+      this.setState({display_current:'CHAT'});
+    }
   }
 
   render() {
-    return(
-      <Auth auth_callback={this.handleAuth.bind(this)}/>
-    )
+    switch (this.state.display_current){
+        case 'AUTH':
+          return(<Auth auth_callback={this.handleAuth.bind(this)}/>);
+          break;
+        case 'CHAT':
+          return(<Chat />);
+          break;
+        default:
+          return(<Auth auth_callback={this.handleAuth.bind(this)}/>);
+          break;
+
+    }
+
   }
 }
 
---
+++ doctor_interface/src/Chat.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { ChatFeed, Message } from 'react-chat-ui'
+import {
+    ListGroup,
+    ListGroupItem,
+    Label,
+    Input,
+    Container,
+    Row,
+    Col,
+} from 'reactstrap';
+
+class Chat extends Component {
+  constructor() {
+    super();
+    this.state = {
+      is_typing:false,
+      font_size:20,
+      messages: [
+        new Message({id: 1, message: "I'm the recipient! (The person you're talking to)",}), // Gray bubble
+        new Message({id: 1, message: "I'm the recipient! (The person you're talking to)",}), // Gray bubble
+        new Message({id: 0, message: "I'm you -- the blue bubble!" }), // Blue bubble
+      ],
+      patients: [
+        {name:'123', ssn:'123123'},
+        {name:'123', ssn:'123123'},
+        {name:'123', ssn:'123123'},
+        {name:'123', ssn:'123123'},
+        {name:'123', ssn:'123123'},
+        
+      ],
+    };
+  }
+
+  render() {
+    const patients = this.state.patients;
+    return(
+      <Container>
+        <Row>
+          <Col xs={{size:3}}>
+            <ListGroup>
+              {patients.map(function(patient, index){
+                return <ListGroupItem>{patient.name} / {patient.ssn}</ListGroupItem>
+              })}
+            </ListGroup>
+          </Col>
+          <Col>
+              <ChatFeed
+                messages={this.state.messages} // Boolean: list of message objects
+                isTyping={this.state.is_typing} // Boolean: is the recipient typing
+                hasInputField={false} // Boolean: use our input, or use your own
+                showSenderName // show the name of the user who sent the message
+                bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+              />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
+}
+
+export default Chat;
