5f4ff0a9ffe981bb0696825150bb38b15ee24db4
lext <aleksei.tiulpin@protonmail.ch>
2018-06-16 03:13:53 +0300

    better train data

--- translation/fetch_train_data.sh
+++ translation/fetch_train_data.sh
@@ -4,17 +4,18 @@ mkdir -p train_data
 chmod -R 755 train_data
 cd train_data
 # Getting the datasets
-#wget https://korp.csc.fi/download/opusparcus/opusparcus_en.zip
-#wget https://korp.csc.fi/download/opusparcus/opusparcus_fi.zip
+wget https://korp.csc.fi/download/opusparcus/opusparcus_en.zip
+wget https://korp.csc.fi/download/opusparcus/opusparcus_fi.zip
 # unpacking
-#unzip opusparcus_en.zip
-#unzip opusparcus_fi.zip
+unzip opusparcus_en.zip
+unzip opusparcus_fi.zip
+
 cd opusparcus_v1
 TO_RETURN=$PWD
-#cd en
-#cd train
-#echo "==> extracting English"
-#bzip2 -dk en-train.txt.bz2
+cd en
+cd train
+echo "==> extracting English"
+bzip2 -dk en-train.txt.bz2
 
 cd $TO_RETURN
 cd fi
